4a318c115a3050b8eda060b472e21db6f04d33ed
andyl <andy@r210.com>
2024-07-23 14:57:52 -0700

    feat: Add install script
    Bash script - makes symlinks for desktop file and launcher script.

--- install.sh
+++ install.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash 
 
-echo "INSTALL SCRIPT - UNDER CONSTRUCTION"
+echo "PLUG_EDITOR_LAUNCH INSTALL"
+
+REPO_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DESK_DIR="$(realpath ~/.local/share/applications)"
+DESKTOP="plug-edit.desktop"
+LAUNCHER="plug_editor_launch"
+
+echo "Symlink /usr/local/bin/$LAUNCHER" 
+sudo ln -fs $REPO_PATH/$LAUNCHER /usr/local/bin/$LAUNCHER
+
+echo "Symlink $DESK_DIR/$DESKTOP"
+ln -fs $REPO_PATH/$DESKTOP $DESK_DIR/$DESKTOP
+
+echo "Update MIME database"
+update-desktop-database $DESK_DIR
+
+echo "Done."
